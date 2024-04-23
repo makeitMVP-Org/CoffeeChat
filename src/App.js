86d6430f83485fc3c23c86b00feebdc54249db4b
Kati Ealey <kati.majorealey@gmail.com>
2024-04-23 10:32:18 -0400

    updates to help with deploy

--- src/App.js
+++ src/App.js
@@ -23,6 +23,8 @@ import connectionsData from "./data/connectionsData";
 
 
 // Import the functions you need from the SDKs you need
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
